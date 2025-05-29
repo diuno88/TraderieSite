a19eca54a162009aec57c4f5b2e097ed8678f799
diuno88 <154478232+diuno88@users.noreply.github.com>
2025-05-30 08:44:32 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const BASE = 'https://3c33-54-180-109-55.ngrok-free.app';
+
+const API_CONFIG = {
+  ItemKinds: `${BASE}/ItemKinds`,
+  ItemList: `${BASE}/ItemList`,
+  MakeTraderieUrl: `${BASE}/MakeTraderieUrl`
+};
