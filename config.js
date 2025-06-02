816b0f36bd544b6951b2e521b339dc384f182a01
diuno88 <154478232+diuno88@users.noreply.github.com>
2025-06-02 16:42:03 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 ///const BASE = 'https://waiver-templates-strength-console.trycloudflare.com';
-const BASE = 'https://rays-monitor-ways-peaceful.trycloudflare.com';
 
+const BASE = 'https://seeing-silence-unit-sciences.trycloudflare.com';
 const API_CONFIG = {
   ItemKinds: `${BASE}/ItemKinds`,
   ItemList: `${BASE}/ItemList`,
   MakeTraderieUrl: `${BASE}/MakeTraderieUrl`
+  selectCategories: `${BASE}/selectCategories`
+  
 };
