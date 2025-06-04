aa7b0a3d270cca6903f34880793d5c2b7221cac9
diuno88 <154478232+diuno88@users.noreply.github.com>
2025-06-04 21:32:11 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 ///const BASE = 'https://waiver-templates-strength-console.trycloudflare.com';
 //python -m http.server 8000
-const BASE = 'https://kb-descending-advertisements-packing.trycloudflare.com';
+const BASE = 'https://flower-hundred-taxes-formal.trycloudflare.com';
 const API_CONFIG = {
   ItemKinds: `${BASE}/ItemKinds`,
   ItemList: `${BASE}/ItemList`,
