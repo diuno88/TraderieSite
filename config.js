9896af097c365875c3b0a4817756c1042838c11e
diuno88 <154478232+diuno88@users.noreply.github.com>
2025-06-04 14:38:16 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 ///const BASE = 'https://waiver-templates-strength-console.trycloudflare.com';
-
-const BASE = 'https://wash-liz-enquiries-scripts.trycloudflare.com';
+//python -m http.server 8000
+const BASE = 'https://kb-descending-advertisements-packing.trycloudflare.com';
 const API_CONFIG = {
   ItemKinds: `${BASE}/ItemKinds`,
   ItemList: `${BASE}/ItemList`,
-  MakeTraderieUrl: `${BASE}/MakeTraderieUrl`
+  MakeTraderieUrl: `${BASE}/MakeTraderieUrl`,
   selectCategories: `${BASE}/selectCategories`
   
 };
