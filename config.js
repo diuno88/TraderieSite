3fd20375b71819dfc24d5324b4feb350ac472d2c
diuno88 <154478232+diuno88@users.noreply.github.com>
2025-06-06 20:36:47 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 ///const BASE = 'https://waiver-templates-strength-console.trycloudflare.com';
 // config.js
+//python -m http.server 8000
+
 const BASE = localStorage.getItem('API_BASE_URL') || 'http://localhost:8000';
 
 const API_CONFIG = {
@@ -7,5 +9,4 @@ const API_CONFIG = {
   ItemList: `${BASE}/ItemList`,
   MakeTraderieUrl: `${BASE}/MakeTraderieUrl`,
   selectCategories: `${BASE}/selectCategories`
-  
 };
