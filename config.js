a5e013c4168f18b49c624417f2322d07a172a9c9
diuno88 <154478232+diuno88@users.noreply.github.com>
2025-06-11 09:23:49 +0900

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 //python -m http.server 8000
 
 //const BASE = localStorage.getItem('API_BASE_URL') || 'http://localhost:8000';
-const BASE = 'https://orders-transcript-repeat-curve.trycloudflare.com';
+const BASE = 'https://med-valuation-seminars-res.trycloudflare.com';
 const API_CONFIG = {
   ItemKinds: `${BASE}/ItemKinds`,
   ItemList: `${BASE}/ItemList`,
