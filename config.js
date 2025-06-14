d74b741bde65520919baa47ee5dd0d944e1601c6
diuno88 <154478232+diuno88@users.noreply.github.com>
2025-06-14 16:30:53 +0900

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 //python -m http.server 8000
 
 //const BASE = localStorage.getItem('API_BASE_URL') || 'http://localhost:8000';
-const BASE = 'https://hq-equipped-memorabilia-sims.trycloudflare.com';
+const BASE = 'https://gmc-encryption-jackson-breaks.trycloudflare.com';
 const API_CONFIG = {
   ItemKinds: `${BASE}/ItemKinds`,
   ItemList: `${BASE}/ItemList`,
