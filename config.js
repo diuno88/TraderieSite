c7b441a50a75d06616cd04cecce2a6b990ab8b7f
diuno88 <154478232+diuno88@users.noreply.github.com>
2025-06-18 15:35:55 +0900

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 //python -m http.server 8000
 
 //const BASE = localStorage.getItem('API_BASE_URL') || 'http://localhost:8000';
-const BASE = 'https://respondents-que-cottage-correction.trycloudflare.com';
+const BASE = 'https://measuring-hr-gratuit-around.trycloudflare.com';
 const API_CONFIG = {
   ItemKinds: `${BASE}/ItemKinds`,
   ItemList: `${BASE}/ItemList`,
